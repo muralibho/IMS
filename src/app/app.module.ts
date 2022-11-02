eb12bc5b98d04820148eb93fdc22d4d46d4ea101
muralibho <murali.ang2018@gmail.com>
2022-11-02 10:47:18 +0530

    page not found implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    DashboardComponent
+    
+  
   ],
   imports: [
     BrowserModule,
